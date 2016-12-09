5f343826b464199482026a02266f1f45f3823d88
Skander Jabouzi <jabouzi@gmail.com>
2016-12-08 20:21:23 -0500

    add new files to project

---
+++ Checklists/Checklists/CheckListViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Checklists
+//
+//  Created by Skander Jabouzi on 2016-12-08.
+//  Copyright © 2016 Skander Jabouzi. All rights reserved.
+//
+
+import UIKit
+
+class CheckListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
